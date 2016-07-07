2dc00b5bb706d656246fd8de2fef46453c36e1ef
Flax163 <goncharov.valia@yandex.ru>
2016-07-07 23:28:00 +0400

    refactoring

--- web/src/main/angular2_quickstart/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-
-import { AppComponent } from './app.component.ts';
-import { APP_ROUTER_PROVIDERS } from './app.routes.ts';
-
-bootstrap(AppComponent, [
-    APP_ROUTER_PROVIDERS
-]);
--- web/src/main/frontend/app/main.ts
+++ web/src/main/frontend/app/main.ts
@@ -2,7 +2,8 @@ import { bootstrap }    from '@angular/platform-browser-dynamic';
 
 import { AuditApplicationComponent } from './audit-app.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 bootstrap(AuditApplicationComponent, [
-    APP_ROUTER_PROVIDERS
+    APP_ROUTER_PROVIDERS, HTTP_PROVIDERS
 ]);
